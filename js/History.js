cf0a6c42e05626efafa6f552aebf40289f5dd778
ranjithkumar4hp <62333197+ranjithkumar4hp@users.noreply.github.com>
2020-07-16 18:07:04 +0530

    L to R redirection issue fixed

--- js/History.js
+++ js/History.js
@@ -89,6 +89,7 @@ function settings_page() {
 
 function settings_network_page() {
     $(".Settings-network-Section").addClass('anim-appear').hide(100);
+    $(".Settings-Section").removeClass("anim-appear");
     // $(".Menu-Section").addClass('anim-txt').show(100).animate({ left: '0px' });
     $(".Settings-Section").addClass('transitionLR-reload').removeClass('anim-re-left-right').show(100);
     history_details.pop();
@@ -97,6 +98,7 @@ function settings_network_page() {
 function Settings_wifi_page() {
     $settingwifisection.addClass('anim-appear').hide(100);
     $("#wifislider").css({ opacity: '0', right: '60px' });
+    $(".Settings-network-Section").removeClass("anim-appear");
     $(".Settings-network-Section").addClass('transitionLR-reload').removeClass('anim-disappear').show(100);
     history_details.pop();
 }
